5cf1f63deb5d6bae5b72e2dc03c1d00889a8832d
guites <gui.garcia67@gmail.com>
2023-04-21 18:30:13 -0300

    Modify script to visualize dataset annotations

--- app/spacy/doc_viz.py
+++ app/spacy/doc_viz.py
@@ -1,29 +1,36 @@
 """Based on https://spacy.io/usage/visualizers#ent"""
-import json
+import random
+import requests
 import spacy
 
 from spacy import displacy
 from spacy.tokens import Span
 from spacy.util import filter_spans
 
-with open('data.json', 'r') as f:
-    data = json.load(f)
+# get dataset from /spacy/dataset endpoint
+response = requests.get("http://localhost:8000/spacy/dataset")
+data = response.json()
 
 nlp = spacy.blank("en")
 
-example = data[7]
-print(f"Example text length: {len(example['text'])}")
+examples = random.choices(data['dataset'], k=30)
 
-doc = nlp.make_doc(example['text'])
-ents = []
+all_docs = []
 
-for start, end, label in example['entities']:
-    span = doc.char_span(start, end, label=label, alignment_mode="contract")
-    if span is None:
-        print("Skipping entity")
-    else:
-        ents.append(span)
-    filtered_ents = filter_spans(ents)
-    doc.ents = filtered_ents
+for example in examples:
+    doc = nlp.make_doc(example['text'])
+    ents = []
 
-displacy.serve(doc, style="ent", auto_select_port=True)
+    for start, end, label in example['entities']:
+        span = doc.char_span(start, end, label=label, alignment_mode="contract")
+        if span is None:
+            print("Skipping entity")
+        else:
+            ents.append(span)
+        filtered_ents = filter_spans(ents)
+        doc.ents = filtered_ents
+    all_docs.append(doc)
+    if len(all_docs) > 30:
+        break
+
+displacy.serve(all_docs, style="ent", auto_select_port=True)
