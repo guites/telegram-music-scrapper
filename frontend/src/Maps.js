fc56ee1caddf7c6a0f674c667702d427c4bc0476
fbgrigolo <fbc.grigolo@gmail.com>
2023-04-29 15:09:49 -0300

    First map

--- frontend/src/Maps.js
+++ frontend/src/Maps.js
@@ -2,14 +2,33 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import { useEffect, useState } from 'react';
 
+import { Icon } from 'leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+
+
+
 export const Maps = () => {
-    const [data, setData] = useState("valor inicial");
-    return (
-        <>
-        <Container>Mapas! {data} </Container>
-        <Button onClick={() => setData("outro valor")}>
-            Clique aqui
-        </Button>
-        </>
-    )
+	const [data, setData] = useState("valor inicial");
+	const myIcon = new Icon({ iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png", iconSize: [26, 42] })
+	return (
+		<>
+			<Container>Mapas! {data} </Container>
+			<Button onClick={() => setData("outro valor")}>
+				Clique aqui
+			</Button>
+
+			<MapContainer style={{ height: 500 }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				<Marker icon={myIcon} position={[51.505, -0.09]}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker>
+			</MapContainer>
+		</>
+	)
 };
