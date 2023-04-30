05a76f24d75854a695e8a2919fad338e0c3150af
guites <gui.garcia67@gmail.com>
2023-04-30 12:53:52 -0300

    Add counting info to artists page

--- frontend/src/Artists.js
+++ frontend/src/Artists.js
@@ -1,4 +1,4 @@
-import { Badge, Button, Container } from 'react-bootstrap';
+import { Badge, Button, Card, Container } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import { read_registered_artists } from './requests';
 import { Header } from './components/Header';
@@ -7,6 +7,7 @@ import './Artists.css';
 
 export const Artists = () => {
     const [artists, setArtists] = useState([]);
+    const [totalMsgs, setTotalMsgs] = useState(0);
     const [countRange, setCountRange] = useState([0, 0]);
     
     const badgeColorCode = (count) => {
@@ -21,19 +22,23 @@ export const Artists = () => {
     };
 
     const getMessageCountRange = (artists_list) => {
+        let totalMessages = 0;
         const counts = artists_list.map((artist) => {
-            return artist.telegram_message_ids.length;
+            const qtt = artist.telegram_message_ids.length;
+            totalMessages += qtt;
+            return qtt;
         });
         const min = Math.min(...counts);
         const max = Math.max(...counts);
-        return [min, max];
+        return [min, max, totalMessages];
     }
 
     const get_telegram_message_artists = async () => {
         const json_artists = await read_registered_artists();
         
-        const [min, max] = getMessageCountRange(json_artists);
+        const [min, max, totalMessages] = getMessageCountRange(json_artists);
         setCountRange([min, max]);
+        setTotalMsgs(totalMessages);
         
         setArtists(json_artists);
     };
@@ -48,6 +53,7 @@ export const Artists = () => {
         <Container>
                 <h1>Artists and Bands</h1>
                 <img style={{maxWidth: "300px", float: "left"}} src={f00nky} alt="Funky Kong" />
+                <Card><Card.Body>Showing <Badge bg="primary">{artists.length}</Badge> artists and bands from <Badge bg="primary">{totalMsgs}</Badge> telegram messages.</Card.Body></Card>
                 {artists.map((artist) => {
                     return <Button className="m-1" variant="light" key={artist.id}>{artist.name} <Badge className={badgeColorCode(artist.telegram_message_ids.length)}>{artist.telegram_message_ids.length}</Badge></Button>
                 })}
